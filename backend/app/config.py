3c76a71460728739fb05f4905353d2eb148ddbdb
Anas Khan <ak6062042@gmail.com>
2026-08-16 16:29:55 +0500

    Corrected config paths for railway deployment.

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,21 +1,26 @@
-from os.path import dirname, join
+from pathlib import Path
 
-BASE_DIR = dirname(__file__)
+BASE_DIR = Path(__file__).resolve().parent         
+BACKEND_DIR = BASE_DIR.parent                          
+
 SENTENCES_PER_CHUNK = 6
-RAW_DIR = join(BASE_DIR, "..\\data\\raw")
-CLEANED_PATH = join(BASE_DIR, "..\\data\\processed\\cleaned\\cleaned.jsonl")
-CHUNKS_PATH = join(BASE_DIR,"..\\data\\processed\\chunks\\chunks.jsonl")
-VECTOR_DB_PATH = join(BASE_DIR,"..\\data\\processed\\vector_db")
+
+RAW_DIR = BACKEND_DIR / "data" / "raw"
+CLEANED_PATH = BACKEND_DIR / "data" / "processed" / "cleaned" / "cleaned.jsonl"
+CHUNKS_PATH = BACKEND_DIR / "data" / "processed" / "chunks" / "chunks.jsonl"
+VECTOR_DB_PATH = BACKEND_DIR / "data" / "processed" / "vector_db"
 COLLECTION_NAME = "nayatel_docs"
+
 TEMPERATURE = 1
 MAX_TOKEN = 1200
 MODEL_VERSION = "gpt-5-mini"
-ENV_PATH = join(BASE_DIR, "../.env")
+
+ENV_PATH = BACKEND_DIR / ".env"
+
 RETRIVE_NEIGHBORS = 0
-RAW_DATA_ROOT_DIR = join(BASE_DIR, "..\\data\\raw")
+RAW_DATA_ROOT_DIR = RAW_DIR
 TOP_K = 5
 SIMILARITY_THRESHOLD = 1.15
 KEEP_CHAT_SESSIONS = 10
 CHUNK_SIZE = 650
 OVERLAP = 80
-
